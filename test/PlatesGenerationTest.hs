d389cc8a444b59d6fcba2e51ddf8e58642a3f14f
Federico Tomassetti <f.tomassetti@gmail.com>
2014-11-24 21:21:03 +0000

    Correct name of test_generateInitialHeighMap in test_generateInitialHeightMap

--- test/PlatesGenerationTest.hs
+++ test/PlatesGenerationTest.hs
@@ -9,7 +9,7 @@ import qualified Data.Array.Repa as R
 import qualified HeightMap.Base as HB
 
 --test_generateInitialHeighMap :: IO ()
-test_generateInitialHeighMap = do
+test_generateInitialHeightMap = do
     heightMap :: HB.HeightMap (HB.Point Float) <- generateInitialHeightMap 1 512 512
     -- I would expect to hae 512x512 points all beteen 0.0 and 1.0
     let points :: [HB.Point Float] = R.toList heightMap
